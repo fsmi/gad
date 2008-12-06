d3063f1ebeae059c57513043766cd88f2cc997fa
Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
2008-12-06 13:13:53 +0100

    debian: Remove cron job.

--- debian/cron.d
+++
@@ -1,7 +0,0 @@
-#
-# Regular cron jobs for the gad package
-#
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-*/15 *   * * *   root    [ -x /usr/sbin/gad ] && /usr/sbin/gad /etc/gad.conf
